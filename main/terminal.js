b10294c11a53b8a6c53b0ba4f00ef2f7dd5ef217
micul01 <valentin.musoiu@gmail.com>
2016-11-22 17:58:30 +0800

    Fixed JSLint warnings

--- main/terminal.js
+++ main/terminal.js
@@ -1,43 +1,56 @@
 
-
-function listen(e) {
-	if (e.keyCode == 13) {
-        interpret(read_command());
-        reset_commandline();
-        return false;
-    }
-}
-
 function get_commandline() {
+    'use strict';
+    
 	return document.getElementById("cmd");
 }
 
 function read_command() {
+    'use strict';
+    
 	return get_commandline().value.trim();
 }
 
 function reset_commandline() {
+    'use strict';
+    
 	get_commandline().value = '';
 }
 
+function getFirstCommand(command) {
+    'use strict';
+    
+	var idx = command.slice(0).indexOf(' '),
+	    word = idx === -1 ? command
+		    : command.substr(0, idx);
+	return word;
+}
+
+function show(command) {
+    'use strict';
+    
+	var results = document.getElementById("results"),
+        para = document.createElement("P"),
+        text = document.createTextNode(command);
+	para.appendChild(text);
+	results.appendChild(para);
+}
+
 function interpret(command) {
+    'use strict';
+    
 	show(command);
 	if (command.length > 0) {
 		show(getFirstCommand(command) + ': command not found');
 	}
 }
 
-function getFirstCommand(command) {
-	var idx = command.slice(0).indexOf(' ');
-	var word = idx == -1 ? command 
-		: command.substr(0, idx);
-	return word;
-}
-
-function show(command) {
-	var results = document.getElementById("results");
-	var para = document.createElement("P"); 
-	var text = document.createTextNode(command); 
-	para.appendChild(text); 
-	results.appendChild(para); 
+function listen(e) {
+    'use strict';
+    
+	if (e.keyCode === 13) {
+        interpret(read_command());
+        reset_commandline();
+        return false;
+    }
 }
