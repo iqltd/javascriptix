474503f3349a4d58d69d9a78dd21429706a2249d
micul01 <valentin.musoiu@gmail.com>
2016-12-16 00:22:41 +0100

    added more cd tests

--- test/js/builtin_test.js
+++ test/js/builtin_test.js
@@ -27,7 +27,30 @@
         cd_home: function () {
             j$.bash.builtins.cd(['cd']);
             assertEquals(j$.context.user.home + "/", j$.context.directory.path());
+        },
+        cd_relative: function () {
+            j$.fs.mkdir("a", j$.fs.get(j$.context.user.home), j$.context.user);
+            j$.bash.builtins.cd(['cd']);
+            j$.bash.builtins.cd(['cd', 'a']);
+            assertEquals(j$.context.user.home + "/a/", j$.context.directory.path());
+        },
+        cd_startWithFullStop: function () {
+            j$.fs.mkdir("a", j$.fs.get(j$.context.user.home), j$.context.user);
+            j$.bash.builtins.cd(['cd']);
+            j$.bash.builtins.cd(['cd', './a']);
+            assertEquals(j$.context.user.home + "/a/", j$.context.directory.path());
+        },
+        cd_file: function () {
+            j$.fs.touch("file", j$.fs.get(j$.context.user.home), j$.context.user);
+            j$.bash.builtins.cd(['cd']);
+            try {
+                j$.bash.builtins.cd(['cd', 'file']);
+                throw new Error("assertion failed");
+            } catch (e) {
+                
+            }
         }
+        
     });
     
 }(window.t$ = window.t$ || {}));
