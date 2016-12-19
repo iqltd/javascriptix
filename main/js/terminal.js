62b181141c9a613c7664cfe93dc7e5f98ce13575
micul01 <micul01@gmail.com>
2016-12-19 18:52:08 +0100

    refactor

--- main/js/terminal.js
+++ main/js/terminal.js
@@ -8,35 +8,30 @@ window.onload = function () {
         return stdin.value.trim();
     }
 
-    function resetPrompt() {
-        prompt.textContent = j$.context.promptString();
+    function resetPrompt(string) {
+        prompt.textContent = string ? string : j$.context.promptString();
         stdin.value = '';
     }
-    
-    function continueOnNextLine() {
-        prompt.textContent = '> ';
-        stdin.value = '';
+
+    function newElement(elementType, classList, textContent, id) {
+        var i, element = document.createElement(elementType);
+        element.textContent = textContent;
+        element.id = id;
+        for (i = 0; i < classList.length; i++) {
+            element.classList.add(classList[i]);
+        }
+        return element;
     }
 
     function show(text, showPromptText) {
-        var line = document.createElement("DIV"),
-            span,
-            info;
-        results.appendChild(line);
-
+        var line = document.createElement('div');
         if (showPromptText) {
-            span = document.createElement("SPAN");
-            span.classList.add("commandText");
-            span.classList.add("promptText");
-            span.textContent = prompt.textContent;
-            line.appendChild(span);
+            line.appendChild(newElement('span', ['commandText', 'promptText'], prompt.textContent));
         }
         if (text) {
-            info = document.createElement("SPAN");
-            info.textContent = text;
-            info.classList.add("preformatted");
-            line.appendChild(info);
+            line.appendChild(newElement('span', ['preformatted'], text));
         }
+        results.appendChild(line);
     }
     
     function listen(e) {
@@ -48,22 +43,11 @@ window.onload = function () {
     
     function buildUi() {
         j$Div = document.getElementById("javascriptix");
-        stdin = document.createElement("TEXTAREA");
-        results = document.createElement("DIV");
-        prompt = document.createElement("SPAN");
-            
-        stdin.id = "stdin";
-        stdin.classList.add("commandText");
-        stdin.classList.add("normalText");
+        stdin = newElement('textarea', ['commandText', 'normalText'], '', 'stdin');
+        results = newElement('div', ['commandText', 'normalText'], '', 'results');
+        prompt = newElement('span', ['commandText', 'promptText'], '', 'prompt');
+
         stdin.addEventListener("keypress", listen);
-
-        results.id = "results";
-        results.classList.add("commandText");
-        results.classList.add("normalText");
-
-        prompt.id = "prompt";
-        prompt.classList.add("commandText");
-        prompt.classList.add("promptText");
         resetPrompt();
         
         j$Div.appendChild(results);
@@ -73,25 +57,20 @@ window.onload = function () {
     
     j$.terminal = {};
     
-    j$.InputWaitError = function () {
-        this.base = Error;
-        this.statementIncomplete = true;
-    };
-    
     j$.terminal.processInput = function (userInput) {
+        var promptString;
         show(userInput, true);
         try {
             show(j$.bash.interpret(userInput));
         } catch (err) {
-            if (err.statementIncomplete) {
-                continueOnNextLine();
-                return;
+            if (err instanceof j$.bash.IncompleteInputError) {
+                promptString = '> ';
+            } else {
+                show(err.message);
+                throw err;
             }
-            show(err.message);
-            throw err;
-        } finally {
-            resetPrompt();
         }
+        resetPrompt(promptString);
     };
     
     j$.init();
