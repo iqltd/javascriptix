cdd2eb28e64185c8fac8e67cff1ad4e747bf8d9f
micul01 <valentin.musoiu@gmail.com>
2017-03-31 17:26:24 +0800

    bash_tokens rewritten in a functional (?) way

--- src/js/bash_tokens.js
+++ src/js/bash_tokens.js
@@ -1,55 +1,83 @@
 
-class Event {
-    constructor(pattern, process) {
-        this.pattern = pattern;
-        this.process = process;
-    }
-
-    matches(text, index) {
-        return this.pattern.includes(text[index]);
-    }
+function either(...fcts) {
+    return (...args) => fcts.some(f => f.apply(null, args));
 }
 
-let firstSingleQuote = new Event('\'');
-let secondSingleQuote = new Event('\'');
-let firstDoubleQuote = new Event('"');
-let secondDoubleQuote = new Event('"');
-let whiteSpace = new Event(' \t');
-let newline = new Event('\n');
-let comment = new Event('#');
-let endOfComment = new Event('\n');
-let escape = new Event('\\');
+function allTrue(...fcts) {
+    return (...args) => fcts.every(f => f.apply(null, args));
+}
 
-let allExceptComments = [firstSingleQuote, firstDoubleQuote, whiteSpace, newline, escape];
-let all = allExceptComments.slice();
-all.push(comment);
+function not(f) {
+    return (...args) => !f.apply(null, args);
+}
 
-firstSingleQuote.allowed = [secondSingleQuote];
-secondSingleQuote.allowed = allExceptComments;
+let Token = function(start, end) {
+    this.start = start;
+    this.end = end;
+    this.startFrom = 0;
+    this.allowed = null;
+};
 
-firstDoubleQuote.allowed = [secondDoubleQuote];
-secondDoubleQuote.allowed = allExceptComments;
+let inputEnds = (s, i) => s.length <= i + 1;
+let commentStarts = (s, i) => s[i] === '#';
+let commentEnds = (s, i) => s[i] === '\n';
+let comment = new Token(commentStarts, either(commentEnds, inputEnds));
 
-whiteSpace.allowed = all;
-newline.allowed = all;
+let isEscaped = (s, i) => s[i-1] === '\\';
+let isSingleQuote = (s, i) => s[i] === '\'';
+let singleQuoted = new Token(allTrue(isSingleQuote, not(isEscaped)), isSingleQuote);
+singleQuoted.startFrom = 1;
 
-comment.allowed = [endOfComment];
-endOfComment.allowed = [all];
+let isDoubleQuote = (s, i) => s[i] === '"';
+let isValidDoubleQuote = allTrue(isDoubleQuote, not(isEscaped));
+let doubleQuoted = new Token(isValidDoubleQuote, isValidDoubleQuote);
+doubleQuoted.startFrom = 1;
 
-function doMe(text) {
-    let i = 0, allowed = all;
+let isWhitespace = (s, i) => ' \t'.includes(s[i]);
+let whitespaceEnds = (s, i) => !' \t'.includes(s[i+1]);
+let whitespace = new Token(isWhitespace, either(whitespaceEnds, inputEnds));
+
+let wordEnds = (s, i) => ' \t'.includes(s[i+1]);
+let word = new Token(not(isWhitespace), either(inputEnds, wordEnds));
+word.allowed = [singleQuoted, doubleQuoted];
+
+function tokenize(text) {
+    let allowed = [comment, singleQuoted, doubleQuoted, whitespace, word];
+    let token = allowed.find(token => token.start(text, 0));
+    let end = findEnd(text, token.startFrom, token);
+    return [text.slice(0, end + 1), text.slice(end + 1)] ;
+}
+
+function findEnd(text, i, token) {
+    if (!token) {
+        return i - 1;
+    }
     while (i < text.length) {
-        for (let event of allowed) {
-            if (event.matches(text, i)) {
-                console.log(`Found char matching ${event.pattern} at position ${i}`);
-                allowed = event.allowed;
-                break;
-            }
+        if (token.end(text, i)) {
+            return i;
         }
-
+        if (token.allowed) {
+            let newToken = token.allowed.find(newToken => newToken.start(text, i));
+            i = findEnd(text, i+1, newToken); 
+        }
         i++;
     }
 }
 
-doMe('012345 "xxxxxxx" #  aaa\n');
+let cases = [
+    'a b',
+    ' b',
+    'b',
+    'abcde',
+    'abc"d"e',
+    'abcd\'e\'',
+    'abc#nocomment de',
+    'abc #comment',
+    '#comment, so it will be treated like a single word',
+    '"double quoted"'
+];
 
+cases.forEach(tc => { 
+    console.log(tc + ' -> '); 
+    console.log(tokenize(tc)); 
+});
